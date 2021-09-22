12eac224f63b76a7c70b9685067939e0ac13c1a5
Shiv Yadav <shiv@cibirix.com>
2021-09-22 09:24:26 +0530

    promise

--- javascrips/tutorial/functions-and-types.php
+++ javascrips/tutorial/functions-and-types.php
@@ -1,5 +1,2 @@
-functions are first class citizens
-in javascript we can assign a function to a variable.
-
-
+functions are first class citizens in javascript we can assign a function to a variable.
 any function that is passed to another function as an argument, is known as callback funtions.
