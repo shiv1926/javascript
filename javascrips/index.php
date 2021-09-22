67d4b97cbea066110cd481cf6e6d0e0b4403d3f4
Shiv Yadav <shiv@cibirix.com>
2021-09-22 09:01:19 +0530

    js index

--- javascrips/index.php
+++ javascrips/index.php
@@ -2,7 +2,6 @@
 <ul class="index">
 	<li><a href="<?php echo jsbasic('introduction.php'); ?>">Introduction</a></li>
 	<li><a href="<?php echo jsbasic('tutorial/how-javascript-works.php'); ?>">How javascript works</a></li>
-	<li>https://www.youtube.com/watch?v=VaBP6_pBOgM&list=PLwGdqUZWnOp0hSGwasCjbrWSPrEKqB_kz, important video from 1 to 10</li>
 	<li>
 		<h4>Storage</h4>
 		<ul>
