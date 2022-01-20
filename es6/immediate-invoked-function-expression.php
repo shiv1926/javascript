9fb158b22a0b05072e1a7c46720e7b458c6e4c67
Shiv Yadav <shiv@cibirix.com>
2022-01-20 17:40:07 +0530

    iife

--- es6/immediate-invoked-function-expression.php
+++ es6/immediate-invoked-function-expression.php
@@ -6,9 +6,11 @@
     <li>It is a similar concept to singleton pattern of oops. </li>
 </ul>
 <?php 
+$links[] = 'https://www.tutorialsteacher.com/javascript/immediately-invoked-function-expression-iife';
 $links[] = 'https://developer.mozilla.org/en-US/docs/Glossary/IIFE';
 $links[] = 'https://www.codeproject.com/Articles/1110916/JavaScript-IIFE-Design-Pattern';
 $links[] = 'https://www.geeksforgeeks.org/javascript-immediately-invoked-function-expressions-iife/';
+$links[] = 'https://stackoverflow.com/questions/37021349/benefit-of-immediately-invoked-function-expression-iife-over-a-normal-function';
 echo refrences($links);
 include('footer.php'); 
 ?>
