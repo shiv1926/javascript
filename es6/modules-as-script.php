ec4703612aa30d06e1778f03538c002c293bda61
Shiv Yadav <shiv@cibirix.com>
2022-02-07 19:47:02 +0530

    module links

---
+++ es6/modules-as-script.php
@@ -0,0 +1,11 @@
+<?php include('header.php'); ?>
+<ul>
+</li>
+</ul>
+<?php 
+$links[] = 'https://javascript.info/modules-intro';
+$links[] = 'https://usefulangle.com/post/252/script-type-module#:~:text=A%20script%20tag%20having%20type,module%20for%20the%20imported%20modules.';
+$links[] = 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Modules';
+echo refrences($links);
+include('footer.php'); 
+?>
